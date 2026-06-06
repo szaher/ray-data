bce77449e66b9ba6cadfd12a54765966c3b2b65a
Saad Zaher <szaher@redhat.com>
2026-06-06 02:08:46 +0100

    feat: wire MermaidDiagram and CodeBlock into MDX components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,14 @@
 import type { MDXComponents } from "mdx/types";
+import MermaidDiagram from "@/components/MermaidDiagram";
+import CodeBlock from "@/components/CodeBlock";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
+    MermaidDiagram,
+    CodeBlock,
+    pre: ({ children, ...props }: React.ComponentPropsWithoutRef<"pre">) => {
+      return <pre {...props}>{children}</pre>;
+    },
   };
 }
